c8156b3c07989ebcaeda4b2a664f0f318f2e404f
Jonas Holbech <jh@jonasholbech.dk>
2018-06-14 23:54:43 +0200

    Prettier, airbnb & eslint running

--- src/App.js
+++ src/App.js
@@ -5,65 +5,65 @@ import Week from './components/Week/';
 import AddTrackForm from './components/AddTrackForm/';
 import Settings from './components/Settings/';
 
-//TODO der skal tegnes meget snart
+// TODO der skal tegnes meget snart
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    if(localStorage.getItem("timelines")){
-      let timelines = localStorage.getItem("timelines");
+    if (localStorage.getItem('timelines')) {
+      let timelines = localStorage.getItem('timelines');
       timelines = JSON.parse(timelines);
-      this.state=timelines;
+      this.state = timelines;
     } else {
-      this.state = this.getInitialData();
+      this.getInitialData();
     }
   }
-  save(){
-    localStorage.setItem("timelines", JSON.stringify(this.state));
-  }
-  getInitialData(){
-    const weeks=[];
+  getInitialData() {
+    const weeks = [];
     const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
-    for(let w=0; w<12; w++){
-      let week=[];
-      for(let d=0; d<5; d++){
+    for (let w = 0; w < 12; w += 1) {
+      const week = [];
+      for (let d = 0; d < 5; d += 1) {
         week.push(days[d]);
       }
       weeks.push(week);
     }
-    const defaultTracks = [//this is bad, duplicate code
-      {name: "git", color: "#bada55"},
-      {name: "tools", color: "#c0ffee"},
-      {name: "HTML", color: "#b000b5"},
-      {name: "CSS", color: "#C55"},
-      {name: "SVG", color: "#FB1"},
-      {name: "JS", color: "#707020"}
-  ];
-  defaultTracks.forEach(track=>{
-    let ar=[];
-    for(let i=0; i<12*5; i++){
-      ar.push({
-        id: i,
-        value: ''
-      });
-    }
-    track.slots= ar;
-  });
-    
+    const defaultTracks = [
+      // this is bad, duplicate code
+      { name: 'git', color: '#bada55' },
+      { name: 'tools', color: '#c0ffee' },
+      { name: 'HTML', color: '#b000b5' },
+      { name: 'CSS', color: '#C55' },
+      { name: 'SVG', color: '#FB1' },
+      { name: 'JS', color: '#707020' }
+    ];
+    defaultTracks.forEach(track => {
+      const ar = [];
+      for (let i = 0; i < 12 * 5; i += 1) {
+        ar.push({
+          id: i,
+          value: ''
+        });
+      }
+      track.slots = ar;
+    });
 
-    return {
+    this.state = {
       tracks: defaultTracks,
       showSettings: false,
-      weeks: weeks,
+      weeks,
       showTrackForm: false
     };
   }
-  showTrackForm(e){
+  save() {
+    localStorage.setItem('timelines', JSON.stringify(this.state));
+  }
+  showTrackForm() {
     this.setState({
       showTrackForm: !this.state.showTrackForm
     });
     this.save();
   }
-  trackAdded(e){
+  trackAdded(e) {
     const tracks = this.state.tracks;
     tracks.push(e);
     this.setState({
@@ -72,59 +72,59 @@ class App extends Component {
     });
     this.save();
   }
-  slotsReordered(newOrder, trackId){
+  slotsReordered(newOrder, trackId) {
     const tracks = this.state.tracks;
-    tracks[trackId].slots=newOrder;
-    this.setState({
-      tracks:tracks
-    });
-    this.save();
-  }
-  slotChanged(tid,sid,newVal){
-    const tracks = this.state.tracks;
-    
-    let index = tracks[tid].slots.findIndex(el=>el.id===sid);
-    tracks[tid].slots[index].value=newVal;
+    tracks[trackId].slots = newOrder;
     this.setState({
       tracks: tracks
     });
     this.save();
   }
-  showSettings(){
+  slotChanged(tid, sid, newVal) {
+    const tracks = this.state.tracks;
+    const index = tracks[tid].slots.findIndex(el => el.id === sid);
+    tracks[tid].slots[index].value = newVal;
+    this.setState({
+      tracks
+    });
+    this.save();
+  }
+  showSettings() {
     const newValue = !this.state.showSettings;
     this.setState({
-      showSettings:newValue
+      showSettings: newValue
     });
-    
   }
   render() {
-    const weeks = this.state.weeks.map((w,i) => <Week key={i} data={w} />);
-    const tracks = this.state.tracks.map((t,i)=><Track 
-      slotsReordered={this.slotsReordered.bind(this)} 
-      slotChanged={this.slotChanged.bind(this)}
-      key={i} 
-      data={t} 
-      tid={i}
-    />);
-    const trackNames = this.state.tracks.map((t,i)=><h3 key={i}>{t.name}</h3>)
+    const weeks = this.state.weeks.map((w, i) => <Week key={i} data={w} />);
+    const tracks = this.state.tracks.map((t, i) => (
+      <Track
+        slotsReordered={this.slotsReordered.bind(this)}
+        slotChanged={this.slotChanged.bind(this)}
+        key={i}
+        data={t}
+        tid={i}
+      />
+    ));
+    const trackNames = this.state.tracks.map((t, i) => <h3 key={i}>{t.name}</h3>);
     return (
       <div className="App">
         <aside>
-          <div onClick={this.showSettings.bind(this)}><i className="fas fa-cogs"></i></div>
+          <div onClick={this.showSettings.bind(this)}>
+            <i className="fas fa-cogs" />
+          </div>
           {this.state.showSettings ? <Settings /> : ''}
           <h3>Tracks</h3>
           <button onClick={el => this.showTrackForm(el)}>Add Track</button>
-          {this.state.showTrackForm ? <AddTrackForm onTrackAdded={el=>this.trackAdded(el)} /> : ''}
+          {this.state.showTrackForm ? (
+            <AddTrackForm onTrackAdded={el => this.trackAdded(el)} />
+          ) : (
+            ''
+          )}
         </aside>
-        <section className="weeks">
-          {weeks}
-        </section>
-        <aside className="trackNames">
-          {trackNames}
-        </aside>
-        <section className="tracks">
-          {tracks}
-        </section>
+        <section className="weeks">{weeks}</section>
+        <aside className="trackNames">{trackNames}</aside>
+        <section className="tracks">{tracks}</section>
         <h4>Notes</h4>
         <ol>
           <li>Click add track to create a new track (like JS/SVG/Tools)</li>
--- src/components/Track/index.js
+++ src/components/Track/index.js
@@ -1,40 +1,45 @@
 import React, { Component } from 'react';
 import './Track.css';
 import Slot from '../Slot/';
-import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
+import { SortableContainer, SortableElement, arrayMove } from 'react-sortable-hoc';
 
-class Track extends Component {
-  hexToRgb(hex) {//TODO external lib
-      // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
-      var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-      hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-          return r + r + g + g + b + b;
-      });
+export default class Track extends Component {
+  hexToRgb(hex) {
+    //TODO external lib
+    // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+    var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+      return r + r + g + g + b + b;
+    });
 
-      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-      return result ? {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result
+      ? {
           r: parseInt(result[1], 16),
           g: parseInt(result[2], 16),
           b: parseInt(result[3], 16)
-      } : null;
+        }
+      : null;
   }
-  onSortEnd = ({oldIndex, newIndex}) => {
+  onSortEnd = ({ oldIndex, newIndex }) => {
     const newOrder = arrayMove(this.props.data.slots, oldIndex, newIndex);
     this.props.slotsReordered(newOrder, this.props.tid);
 
     /*this.setState({
       slots: arrayMove(this.state.slots, oldIndex, newIndex),
     });*/
-  }
+  };
   render() {
-    const color=this.hexToRgb(this.props.data.color);
+    const color = this.hexToRgb(this.props.data.color);
     const style = {
       backgroundColor: `rgba(${color.r},${color.g},${color.b},0.3)`
-    }
-    const SortableItem = SortableElement(({value}) =>{
-      return <Slot tid={this.props.tid} slotChanged={this.props.slotChanged.bind(this)} text={value}/>
+    };
+    const SortableItem = SortableElement(({ value }) => {
+      return (
+        <Slot tid={this.props.tid} slotChanged={this.props.slotChanged.bind(this)} text={value} />
+      );
     });
-    const SortableList = SortableContainer(({slots}) => {
+    const SortableList = SortableContainer(({ slots }) => {
       return (
         <div className="Track" style={style}>
           {slots.map((value, index) => (
@@ -43,13 +48,15 @@ class Track extends Component {
         </div>
       );
     });
-    
+
     return (
-      
-        <SortableList pressDelay={100} useWindowAsScrollContainer={true} axis="x" slots={this.props.data.slots} onSortEnd={this.onSortEnd} />
-      
+      <SortableList
+        pressDelay={100}
+        useWindowAsScrollContainer={true}
+        axis="x"
+        slots={this.props.data.slots}
+        onSortEnd={this.onSortEnd}
+      />
     );
   }
 }
-
-export default Track;
